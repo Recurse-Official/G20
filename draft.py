47a7727c4c4c8d6cbc47116c8539bd2167ffabc2
Brindanjali <imbrindanjali2403@gmail.com>
2024-12-01 00:10:00 +0530

    Update draft.py

--- draft.py
+++ draft.py
@@ -66,7 +66,7 @@ return img
    
   def extract_driving_license(text):
  driving_license_pattern = r "\b[A-Z]{2}[- ]?d{2}[- ]?\d{7,13}\b"
-  match = re.search(dob_pattern,text) 
+  match = re.search(driving_license_pattern,text) 
   return match. group (0) if match else None
 
   def mask_driving_license (image,driving_license):
@@ -83,7 +83,24 @@ return img
     img =cv2.rectangle(img , (x,y) , (x+w,y+h) , (0,0,0) , -1)
 return img
 
-    
+      def extract_voter_id(text):
+ driving_license_pattern = r "\b[A-Z]{3}\b{7}\b"
+  match = re.search(driving_license_pattern,text) 
+  return match. group (0) if match else None
+
+  def mask_voter_id (image,voter_id):
+  text_data = pytesseract.image_to_date ( img, output_type =pytesseract.output.DICT)
+  for i,word in enumerate (text_data{"text"})
+  if word ==voter_id:
+    x,y,w,h= 
+    (
+      text_data["left"][i],
+      text_data["top"][i],
+      text_data["width"][i],
+      text_data["height"][i],
+    )
+    img =cv2.rectangle(img , (x,y) , (x+w,y+h) , (0,0,0) , -1)
+return img
 
 
 

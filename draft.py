feecfcff41434deca3dbb7a01bd07410cc8e71a0
Meenusree Vanaparthi <meenusreevanaparthi@gmail.com>
2024-11-30 19:03:03 +0530

    Update draft.py

--- draft.py
+++ draft.py
@@ -13,15 +13,14 @@ def extract_aadhaar_number (text):
 
 def mask_aadhaar_number (image,aadhaar_number):
   text_data = pytesseract.image_to_date ( img, output_type =pytesseract.output.DICT)
-  fir i,word in enumerate (text_data{"text"})
-  ptint("Work" , work)
-  x,y,w,h= (
-    text_data["left"][i]
-    text_data["top"][i]
-    text_data["width"][i]
-    text_data["height"][i],
-  )
-  
+  for i,word in enumerate (text_data{"text"})
+  if word in aadhar_no & len(word)==4:
+    x,y,w,h= (
+      text_data["left"][i],
+      text_data["top"][i],
+      text_data["width"][i],
+      text_data["height"][i],
+    )
 img =cv2.rectangle(img, (x,y) , (x+w,y+h), (0,0,0), -1)
 return img
     

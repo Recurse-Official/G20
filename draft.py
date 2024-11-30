12c71ec6d510d2e239c5eb406c1655bf66c5e272
Brindanjali <imbrindanjali2403@gmail.com>
2024-11-30 18:46:36 +0530

    Update draft.py

--- draft.py
+++ draft.py
@@ -5,4 +5,36 @@ import numpy as np
 from PIL import Image
 import streamlit as st
 
+pytessearct.pytesseract.tesseract_cmd= r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+def extract_aadhaar_number (text):
+  aadhaar_pattern =r"\b\d{4}[- ]?\d{4}[- ]?d{4}\b"
+  match = re.search(aadhaar_pattern,text)
+  return match.group (0) if match else none
+
+def mask_aadhaar_number (image,aadhaar_number):
+  text_data = pytesseract.image_to_date ( img, output_type =pytesseract.output.DICT)
+  
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
 
